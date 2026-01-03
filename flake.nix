ea0cdb2142f060cda25f6b4897f85bd33168243c
jossephus <jossephustuemay@gmail.com>
2026-01-03 10:29:02 +0300

    fix: correct libplacebo download URL

--- flake.nix
+++ flake.nix
@@ -124,7 +124,7 @@
       "sources": [
         {
           "type": "archive",
-          "url": "https://github.com/haasn/libplacebo/releases/download/v6.338.2/libplacebo-6.338.2.tar.gz",
+          "url": "https://github.com/haasn/libplacebo/archive/refs/tags/v6.338.2/libplacebo-6.338.2.tar.gz",
           "sha256": "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
         }
       ]
